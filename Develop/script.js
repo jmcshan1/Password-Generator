e3ad6e2d12dd5a227247cccfe050b6ce92f7debc
jmcshan1 <jmcshan01@gmail.com>
2023-06-14 16:35:14 -0500

    added var to store passwordlength, if statement to check if length meets right conditions

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,20 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword(){
+
+  var passwordLength = window.prompt("How many character would you like your password to contain?");
+
+  if (!(8 <= passwordLength && passwordLength <= 128)){
+    window.alert("Pick a number of at least 8 and no more than 128.");
+    generatePassword();
+  };
+
+
+
 function writePassword() {
+  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
